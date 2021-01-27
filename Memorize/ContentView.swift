17dc6d4c193bd4f1f91799a535abe2cdd9e05f17
Deirdre Saoirse Moen <deirdre@deirdre.net>
2021-01-26 23:39:48 -0800

    End of lecture 1.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 3.0)
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill()
+            }
+        }
+        .foregroundColor(Color.orange)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
     }
 }
 
