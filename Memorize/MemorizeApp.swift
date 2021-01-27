1833708ba1f9750a9a18b57010e0ade9987e995e
Deirdre Saoirse Moen <deirdre@deirdre.net>
2021-01-26 21:38:19 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Deirdre Saoirse Moen on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
